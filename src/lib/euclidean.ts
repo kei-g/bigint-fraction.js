38cd20cfc9346525ffe895737ac23f99104b7ec2
kei-g <km.8k6ce+github@gmail.com>
2021-09-03 14:15:28 +0900

    :zap: Improve Euclidean Algorithm
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- src/lib/euclidean.ts
+++ src/lib/euclidean.ts
@@ -17,7 +17,7 @@ namespace BigInt {
    */
   export const abs =
     (value: bigint): bigint =>
-      value < 0n ? -value : value
+      value < 0 ? -value : value
 }
 
 /**
@@ -40,15 +40,18 @@ export namespace Euclidean {
    * A greatest common divisor of two arguments.
    */
   export const GCD = (a: bigint, b: bigint): bigint => {
-    if (a === 0n || b === 0n)
+    if (!a || !b)
       return 0n
-    let [m, n] = [a, b].map(BigInt.abs)
-    while (n) {
-      const r = m % n
-      m = n
-      n = r
-    }
-    return m
+    if (a === b)
+      return a
+    const m = BigInt.abs(a)
+    const n = BigInt.abs(b)
+    if (m < n)
+      return compute(n, m)
+    else if (m === n)
+      return m
+    else
+      return compute(m, n)
   }
 
   /**
@@ -68,22 +71,52 @@ export namespace Euclidean {
    */
   export const GCDAsync =
     (a: bigint, b: bigint): Promise<bigint> =>
-      a === 0n || b === 0n
+      !a || !b
         ? Promise.resolve(0n)
-        : new Promise(
-          (
-            resolve: (value: bigint) => void
-          ) =>
-            setImmediate(
-              () => {
-                let [m, n] = [a, b].map(BigInt.abs)
-                while (n) {
-                  const r = m % n
-                  m = n
-                  n = r
-                }
+        : a === b
+          ? Promise.resolve(a)
+          : new Promise(
+            (
+              resolve: (value: bigint) => void
+            ) => {
+              const m = BigInt.abs(a)
+              const n = BigInt.abs(b)
+              if (m < n)
+                computeLazily(n, m, resolve)
+              else if (m === n)
                 resolve(m)
-              }
-            )
-        )
+              else
+                computeLazily(m, n, resolve)
+            }
+          )
 }
+
+const compute =
+  (
+    greater: bigint,
+    lesser: bigint,
+  ): bigint => {
+    for (; ;) {
+      const r = greater % lesser
+      if (!r)
+        return lesser
+      greater = lesser
+      lesser = r
+    }
+  }
+
+const computeLazily =
+  (
+    greater: bigint,
+    lesser: bigint,
+    resolve: (gcf: bigint) => void,
+  ): void => {
+    const r = greater % lesser
+    if (r)
+      setImmediate(computeLazily, lesser, r, resolve)
+    else
+      resolve(lesser)
+  }
+
+Object.freeze(compute)
+Object.freeze(computeLazily)
--- test/euclidian.spec.ts
+++ test/euclidian.spec.ts
@@ -9,11 +9,17 @@ describe('euclidean', () => {
   it('async gcd(12, 15)', async () =>
     expect(await Euclidean.GCDAsync(12n, 15n)).to.eq(3n)
   )
+  it('async gcd(1, -1) === 1', async () =>
+    expect(await Euclidean.GCDAsync(1n, -1n)).to.eq(1n)
+  )
   it('gcd(6, 9) === 3', () =>
     expect(Euclidean.GCD(6n, 9n)).to.eq(3n)
   )
   it('gcd(-30, 105) === 15', () =>
     expect(Euclidean.GCD(-30n, 105n)).to.eq(15n)
+  )
+  it('gcd(1, -1) === 1', () =>
+    expect(Euclidean.GCD(1n, -1n)).to.eq(1n)
   )
   it('gcd(0, 0) === 0', () =>
     expect(Euclidean.GCD(0n, 0n)).to.eq(0n)
